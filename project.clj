d3265a8601bd9cb5170339b28528cf179c9c16cc
Andreas Zimpel <mail4andz@gmail.com>
2018-12-17 22:13:43 +0100

    Override release-tasks to use lein-v

--- project.clj
+++ project.clj
@@ -3,6 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :release-tasks [["vcs" "assert-committed"]
+                  ["v" "update"]
+                  #_["vcs" "push"]
+                  #_["deploy"]]
   :plugins [[com.roomkey/lein-v "6.4.0"]
             [io.aviso/pretty "0.1.35"]]
   :dependencies [[org.clojure/clojure "1.10.0"]
