0abe5283b766a92c8f9ed7c5989068d8f2f83a6b
Andreas Zimpel <mail4andz@gmail.com>
2019-01-03 23:52:01 +0100

    Parse the new Clojure Community site HTML structure
    - parsed new list instead of the previous table

--- dev-src/user.clj
+++ dev-src/user.clj
@@ -13,30 +13,35 @@
       h/parse
       h/as-hickory))
 
-(defn table-rows [hdoc]
-  (-> (sel/descendant (sel/tag :tbody)
-                      (sel/tag :tr))
+(defn select-entries [hdoc]
+  (-> (sel/descendant (sel/class :ulist)
+                      (sel/tag :ul)
+                      (sel/tag :li))
       (sel/select hdoc)))
 
-(defn name-url [hrow]
-  (-> (sel/descendant (sel/tag :td)
-                      (sel/tag :p))
-      (sel/select hrow)))
+(defn entry [h-entry]
+  (-> (sel/descendant (sel/tag :p)
+                      (sel/tag :a))
+      (sel/select h-entry)))
 
 (def content-str (comp clojure.string/trim first :content))
-
-(defn content [htuple]
-  (mapv content-str htuple))
+(def a-href (comp :href :attrs))
 
 (comment
 
+  (def sample-anchor
+    {:type :element, :attrs {:href "http://8thlight.com/", :rel "nofollow"}, :tag :a, :content ["8th Light"]})
+
+  (a-href sample-anchor)
 
   (->> (url->hickory cc-url)
-       (table-rows)
-       (map name-url)
-       (map content)
-       (count)
-       #_(run! println))
+       (select-entries)
+       #_(take 1)
+       (mapcat entry)
+       (map (juxt content-str a-href))
+       (map (fn [[n url]] (hash-map :company-name n, :company-url url)))
+       #_(count)
+       (run! println))
 
   ;; keep newline
   )
