c244dd2a2b215bfcd913ffa2f3d33884e4331c7b
Andreas Zimpel <mail4andz@gmail.com>
2019-01-03 23:50:04 +0100

    Update dependencies, declare explicit lein middleware
    - added explicit middleware declaration required by Leiningen v2.8.3

--- project.clj
+++ project.clj
@@ -16,17 +16,29 @@
    #_["deploy"]]
 
   :plugins [[com.roomkey/lein-v "6.4.0"]
-            [io.aviso/pretty "0.1.35"]]
+            [io.aviso/pretty "0.1.36"]]
 
   :dependencies
   [[aleph "0.4.6"]
    [byte-streams "0.2.4"]
    [hickory "0.7.1"]
    [org.clojure/clojure "1.10.0"]
-   [io.aviso/pretty "0.1.35"]]
+   [io.aviso/pretty "0.1.36"]]
+
+  :middleware
+  [lein-v.plugin/middleware
+   pretty.plugin/middleware]
 
   :profiles
   {:uberjar {:aot :all},
    :dev
    {:source-paths ["dev-src"],
-    :repl-options {:init-ns user}}})
+    :repl-options {:init-ns user},
+    :middleware
+    [cider-nrepl.plugin/middleware
+     refactor-nrepl.plugin/middleware],
+    :dependencies
+    [[nrepl "0.5.3"]],
+    :plugins
+    [[refactor-nrepl "2.4.0"]
+     [cider/cider-nrepl "0.20.0-SNAPSHOT"]]}})
